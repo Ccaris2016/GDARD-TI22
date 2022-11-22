04103dff03cb8e41b0c55ddbda1874f8851aeaff
Daniel Cotal <dcotal2021@alu.uct.cl>
2022-11-21 22:15:04 -0300

    Metodo Delete más verificación de token

--- Gdard_ti22/app/Http/Controllers/DeleteController.php
+++ Gdard_ti22/app/Http/Controllers/DeleteController.php
@@ -6,16 +6,30 @@ use Illuminate\Support\Facades\DB;
 
 class DeleteController extends Controller
 {
-    public function methodDelete($api,$tabla,$columns,$parametros){ 
+    public function methodDelete(Request $request,$api,$tabla,$columns,$parametros){ 
         function FunctionName($parametro)
         {
             if(is_string($parametro)){
                 $parametro = "'". $parametro . "'";
             }
-            return $parametro ;
+            return $parametro;
         }
-        $dato  = FunctionName($parametros) ;
-        $query = DB::delete('delete from '.$tabla.' where '.$columns.' = '.$dato);
-        return $query;
+        function Callquery($api,$tabla,$columns,$parametros){
+            $dato  = FunctionName($parametros) ;
+            $query = DB::delete('delete from '.$tabla.' where '.$columns.' = '.$dato);
+            return $query;
+        }
+
+        $vToken = DB::select('Select remember_token from users');
+        //$vToken = (array) $vToken;
+        $rToken = $request->input('token');
+        $aToken = (array) $rToken;
+        foreach ($vToken as $token) {
+            //$token = $token["remember_token"];
+            if ($token->remember_token == $aToken[0]) {
+                return Callquery($api,$tabla,$columns,$parametros);
+            }
+        }
+        return 'Token inválido';
     }
 }
